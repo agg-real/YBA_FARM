a7d85cd25d62ebd5993f45eacbdfdde58cbc23e3
agg-real <agg603450@gmail.com>
2025-08-28 18:02:52 +0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -139,7 +139,7 @@ local function QLMOT_fake_script() -- ScreenGui.LocalScript
 					while item:IsDescendantOf(game.Workspace) and tpOn do
 						local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
 						travelTo(item)
-						if (item.Position - plr.Position).Magnitude < 45 then
+						if (item.Position - plr.Position).Magnitude < 95 then
 							maxItems()
 							fireproximityprompt(proxPrompt, 4)
 						end
